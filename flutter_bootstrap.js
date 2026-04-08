52c9e5ce5b447a0a4bfc05ce8b83d7d9a4a9fb87
zylen <zylenw97@gmail.com>
2026-04-08 10:49:57 +0800

    deploy: CNKI panel + tier labels

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2670917186" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "773966090" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
